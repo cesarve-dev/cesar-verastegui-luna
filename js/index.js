11f9a06a44f8cbd4f0c44d78c58dcd3d833a4ee1
cesarve-dev <cesar.radiate@gmail.com>
2025-09-25 00:29:24 -0700

    Add skill section. Create list of skills.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+// add a footer element
+
 const body = document.body;
 body.append(document.createElement('footer'));
+
+// Insert copyright text in footer
 
 const footer = document.querySelector('footer');
 const today = new Date();
@@ -7,5 +11,16 @@ const thisYear = today.getFullYear();
 const copyright = document.createElement('p');
 copyright.innerHTML = "Cesar D. Verastegui " + "&copy" + thisYear;
 
-
 footer.append(copyright);
+
+// Create List of Skills
+
+const skills = ["HTML", "CSS", "Javascript", "Figma", "Webflow", "GitHub"];
+const skillSection = document.getElementById("skills");
+const skillList = skillSection.querySelector("ul");
+
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement('li');
+    skill.innerHTML= skills[i];
+    skillList.appendChild(skill);
+}
