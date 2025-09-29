068005d3146c6e78a143cf67483b0d2c01271429
cesarve-dev <cesar.radiate@gmail.com>
2025-09-29 16:52:00 -0700

    erase duplicate nav properties. Change header top 0% to 0 property. Add semicolon to copyright symbool for better HTML symbol structure.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const footer = document.querySelector('footer');
 const today = new Date();
 const thisYear = today.getFullYear();
 const copyright = document.createElement('p');
-copyright.innerHTML = "Cesar D. Verastegui " + "&copy" + thisYear;
+copyright.innerHTML = "Cesar D. Verastegui " + "&copy; " + thisYear;
 
 footer.append(copyright);
 
